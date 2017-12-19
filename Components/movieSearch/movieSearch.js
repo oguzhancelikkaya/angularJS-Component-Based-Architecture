78665710b77ce74ef69c76b22639e7e83c9095d6
Oğuzhan ÇELİKKAYA <oguzhancelikkaya@gmail.com>
2017-12-19 21:07:54 +0300

    commit 9

--- Components/movieSearch/movieSearch.js
+++ Components/movieSearch/movieSearch.js
@@ -5,19 +5,56 @@ function MovieSearchController($scope, $element, $attrs, $rootScope, $state, $st
   ctrl.imdb = $stateParams.imdb;
   
   ctrl.searchMovie = function() {
-	var params = {
+	 var params = {
      name: ctrl.name,
      year: ctrl.year,
      imdb: ctrl.imdb,
     }
-	$state.go('moviesearch.list', params, {reload: true, notify: false});
+	 $state.go('moviesearch.list', params, {reload: true, notify: false});
   };
   
   ctrl.resetParams = function() {
-	ctrl.name = "";
-	ctrl.year = "";
-	ctrl.imdb = "";	
+	 ctrl.name = "";
+	 ctrl.year = "";
+	 ctrl.imdb = "";	
+    $state.go('moviesearch', {});
   };
+
+    ctrl.deleteMovie = function(movie) {
+    var idx = ctrl.list.indexOf(movie);
+    if (idx >= 0) {
+      ctrl.list.splice(idx, 1);
+    }
+  toaster.pop({type: 'success',title: 'Deleted',body: 'Movie has been deleted',bodyOutputType: 'trustedHtml',timeout: 3000,showCloseButton: true});
+  };
+
+  ctrl.updateMovie = function(movie) {
+    var idx = ctrl.list.indexOf(movie);
+    if (idx >= 0) {
+      ctrl.list[idx].editMode = true;
+    }
+  };
+
+  ctrl.cancelMovie = function(movie, movieCopy) {
+    var idx = ctrl.list.indexOf(movie);
+    if (idx >= 0) {
+      ctrl.list[idx].editMode = false;
+      ctrl.list[idx] = movieCopy;
+    }
+  };
+
+  ctrl.saveMovie = function(movie) {
+  if(helperUtils.isNotEmpty(movie.name)){
+    var idx = ctrl.list.indexOf(movie);
+    if (idx >= 0) {
+    ctrl.list[idx].editMode = false;
+    }
+    toaster.pop({type: 'success',title: 'Updated',body: 'Movie has been updated',bodyOutputType: 'trustedHtml',timeout: 3000,showCloseButton: true});
+  }
+  else{
+    toaster.pop({type: 'warning',title: 'Empty',body: 'Movie name cannot be empty',bodyOutputType: 'trustedHtml',timeout: 3000,showCloseButton: true});
+  }
+  };  
 }
 
 movieApp.component('movieSearch', {
--- Components/movieSearchList/movieSearchList.js
+++ Components/movieSearchList/movieSearchList.js
@@ -1,6 +1,51 @@
-function MovieSearchListController($state, $stateParams) {
+function MovieSearchListController($state, $stateParams, $rootScope, helperUtils, toaster) {
+  var ctrl = this;
+  ctrl.list = [];
+  if(helperUtils.isNotEmpty($stateParams.name) || helperUtils.isNotEmpty($stateParams.year) || helperUtils.isNotEmpty($stateParams.imdb)){
+  	angular.forEach($rootScope.list, function(roll, key){
+  		if(roll.name == $stateParams.name)
+  			ctrl.list[ctrl.list.length] = roll;
+  		else if(roll.year == $stateParams.year)
+  			ctrl.list[ctrl.list.length] = roll;
+  		else if(roll.imdb == $stateParams.imdb)
+  			ctrl.list[ctrl.list.length] = roll;
+  	});
+  	
+  }
 
- 
+  ctrl.deleteMovie = function(movie) {
+    if (ctrl.list.indexOf(movie) >= 0) { ctrl.list.splice(ctrl.list.indexOf(movie), 1); }
+    if ($rootScope.list.indexOf(movie) >= 0) { $rootScope.list.splice($rootScope.list.indexOf(movie), 1); }
+	toaster.pop({type: 'success',title: 'Deleted',body: 'Movie has been deleted',bodyOutputType: 'trustedHtml',timeout: 3000,showCloseButton: true});
+  };
+
+    ctrl.updateMovie = function(movie) {
+    var idx = ctrl.list.indexOf(movie);
+    if (idx >= 0) {
+      ctrl.list[idx].editMode = true;
+    }
+  };
+
+  ctrl.cancelMovie = function(movie, movieCopy) {
+    var idx = ctrl.list.indexOf(movie);
+    if (idx >= 0) {
+      ctrl.list[idx].editMode = false;
+      ctrl.list[idx] = movieCopy;
+    }
+  };
+
+  ctrl.saveMovie = function(movie) {
+	if(helperUtils.isNotEmpty(movie.name)){
+		var idx = ctrl.list.indexOf(movie);
+		if (idx >= 0) {
+		ctrl.list[idx].editMode = false;
+		}
+		toaster.pop({type: 'success',title: 'Updated',body: 'Movie has been updated',bodyOutputType: 'trustedHtml',timeout: 3000,showCloseButton: true});
+	}
+	else{
+		toaster.pop({type: 'warning',title: 'Empty',body: 'Movie name cannot be empty',bodyOutputType: 'trustedHtml',timeout: 3000,showCloseButton: true});
+	}
+  };  
 }
 
 movieApp.component('movieSearchList', {
